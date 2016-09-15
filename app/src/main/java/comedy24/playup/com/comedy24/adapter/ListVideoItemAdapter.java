4fdf52b1af1e7874240ef52314b41817c5727316
hieplq <lequanghiep74@gmail.com>
2016-09-15 20:37:53 +0700

    list view

--- app/src/main/java/comedy24/playup/com/comedy24/adapter/ListVideoItemAdapter.java
+++ app/src/main/java/comedy24/playup/com/comedy24/adapter/ListVideoItemAdapter.java
@@ -1,15 +1,17 @@
 package comedy24.playup.com.comedy24.adapter;
 
 import android.app.Activity;
+import android.os.Build;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import comedy24.playup.com.comedy24.object.ListViewVideoItem;
 import comedy24.playup.com.comedy24.R;
+import comedy24.playup.com.comedy24.object.ListViewVideoItem;
 
 /**
  * Created by lequanghiep on 9/15/2016.
@@ -43,8 +45,14 @@ public class ListVideoItemAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = context.getLayoutInflater();
         view = inflater.inflate(R.layout.list_video_item, null);
+        RelativeLayout relativeLayout = (RelativeLayout) view.findViewById(R.id.video_item);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                relativeLayout.setBackground(context.getDrawable(R.drawable.girl));
+            }
+        }
         TextView textView = (TextView) view.findViewById(R.id.video_info);
-        textView.setText(Html.fromHtml("<b>View:</b> " + listViewVideoItems[i].getView() + "</br>" +
+        textView.setText(Html.fromHtml("<b>View:</b> " + listViewVideoItems[i].getView() + "<br>" +
                 "<b>Time:</b> " + listViewVideoItems[i].getTime()));
         return view;
     }
