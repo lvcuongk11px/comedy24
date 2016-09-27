a64008a39bfdf302ee8afe5898b1903445479d04
hieplq <lequanghiep74@gmail.com>
2016-09-27 22:07:01 +0700

    edit title

--- app/src/main/java/comedy24/playup/com/comedy24/activity/MainActivity.java
+++ app/src/main/java/comedy24/playup/com/comedy24/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentTabHost;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity
 
         context = getApplicationContext();
         initTab();
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Override
